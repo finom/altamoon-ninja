87c80bf754880cc01e806219eb543306f1f9b4b3
Andrey Gubanov <andrey.a.gubanov@gmail.com>
2021-11-23 15:51:10 +0200

    feat: Add some boilerplate code

--- src/index.tsx
+++ src/index.tsx
@@ -1,36 +1,27 @@
 import * as t from 'altamoon-types';
 import React from 'react';
-
+import { Provider } from 'use-change';
 import { render } from 'react-dom';
-import HelloWorld from './HelloWorld';
+import Ninja from './Ninja';
+import NinjaStore from './store';
 
-window.altamoonPlugin((store: t.RootStore) => {
+window.altamoonPlugin((store: t.RootStore & { ninja: NinjaStore }) => {
   const { currentScript } = document;
   if (!currentScript) throw new Error('Unable to detect currentScript');
-  const {
-    element, settingsElement, listenSettingsSave, listenSettingsCancel,
-  } = store.customization.createWidget({
-    id: 'hello_world_react',
-    hasSettings: true,
-    title: 'Hello World React',
+  const { element } = store.customization.createWidget({
+    id: 'altamoon_ninja',
+    hasSettings: false,
+    title: 'Ninja',
     currentScript,
     layout: { h: 6, w: 4, minH: 5 },
   });
 
-  const createOrder = async (side: 'BUY' | 'SELL', quantity: number) => {
-    await store.trading.marketOrder({
-      quantity, side, symbol: store.persistent.symbol,
-    });
-  };
-
-  if (!settingsElement) throw new Error('Settings element is missing even though "hasSettings" is "true"');
+  // eslint-disable-next-line no-param-reassign
+  store.ninja = new NinjaStore(store);
 
   render((
-    <HelloWorld
-      settingsElement={settingsElement}
-      createOrder={createOrder}
-      listenSettingsSave={listenSettingsSave}
-      listenSettingsCancel={listenSettingsCancel}
-    />
+    <Provider value={store}>
+      <Ninja />
+    </Provider>
   ), element);
 });
