5cccc3114efb3c27809f7f02fab86b5dd182a1e6
Andrey Gubanov <andrey.a.gubanov@gmail.com>
2021-11-23 20:52:25 +0200

    fix: Use smoozCandles

--- src/store.ts
+++ src/store.ts
@@ -55,7 +55,7 @@ export default class NinjaStore {
   };
 
   #handle = (origCandles: api.FuturesChartCandle[]): void => {
-    const candles = NinjaStore.candlesToHeikinAshi(origCandles);
+    const candles = NinjaStore.smoozCandles(origCandles);
     const lastCandle = candles[candles.length - 1];
     const { symbol } = this.#store.persistent;
 
@@ -86,6 +86,8 @@ export default class NinjaStore {
     }
   };
 
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   private static candlesToHeikinAshi = (candles: api.FuturesChartCandle[]) => {
     const newCandles: api.FuturesChartCandle[] = [];
     for (let i = 0; i < candles.length; i += 1) {
@@ -109,6 +111,63 @@ export default class NinjaStore {
         low: newLow,
         direction: +newOpen <= +newClose ? 'UP' : 'DOWN',
       };
+    }
+
+    return newCandles;
+  };
+
+  /**
+   * Returns an array of smoothed candles.
+   * (Based on heikin ashi candles, but keeps the real high & low)
+   */
+  private static smoozCandles = (
+    candles: api.FuturesChartCandle[],
+  ): api.FuturesChartCandle[] => {
+    const newCandles: api.FuturesChartCandle[] = [];
+
+    for (let i = 0; i < candles.length; i += 1) {
+      const {
+        open, close, high, low,
+      } = candles[i];
+      const previous = newCandles[i - 1] as api.FuturesChartCandle | undefined;
+
+      let newOpen = previous
+        ? (+previous.open + +previous.close) / 2
+        : (open + close) / 2;
+      let newClose = (open + close + high + low) / 4;
+
+      const newDirection = (newOpen <= newClose)
+        ? 'UP' : 'DOWN';
+
+      // Clamp new open to low/high
+      newOpen = newDirection === 'UP'
+        ? Math.max(newOpen, low)
+        : Math.min(newOpen, high);
+
+      // Keep last candle close as vanilla (to visually keep track of price)
+      if (i === candles.length - 1) {
+        newClose = +close;
+      }
+
+      newCandles.push({
+        ...candles[i],
+        direction: newDirection,
+        open: newOpen,
+        close: newClose,
+      });
+
+      // Adjust close/open of previous candle, we don't want gaps
+      if (previous) {
+        if (newDirection === previous.direction) {
+          previous.close = (previous.direction === 'UP')
+            ? Math.max(previous.close, newOpen)
+            : Math.min(previous.close, newOpen);
+        } else {
+          previous.open = (previous.direction === 'DOWN')
+            ? Math.max(previous.open, newOpen)
+            : Math.min(previous.open, newOpen);
+        }
+      }
     }
 
     return newCandles;
